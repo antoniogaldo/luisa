f317b24e2a5d9388e5e1624417413b9d0b33bd35
antoniogaldo <antoniohendrix@hotmail.it>
2017-11-24 12:32:06 +0100

    responsive index

--- src/Luisa/BackendBundle/Entity/Articoli.php
+++ src/Luisa/BackendBundle/Entity/Articoli.php
@@ -21,6 +21,29 @@ class Articoli
      */
     private $id;
 
+    /**
+    * @ORM\Column(type="string")
+    */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Application\Sonata\MediaBundle\Entity\Media")
+     * @ORM\JoinColumn(name="image_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    private $image;
+
+    /**
+    * @ORM\Column(type="string")
+    */
+    private $link;
+
+    /**
+    * @var text
+    *
+    * @ORM\Column(name="descrizione",type="text")
+    */
+    private $descrizione;
+
 
     /**
      * Get id
@@ -30,5 +53,101 @@ class Articoli
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Articoli
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     *
+     * @return Articoli
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set descrizione
+     *
+     * @param string $descrizione
+     *
+     * @return Articoli
+     */
+    public function setDescrizione($descrizione)
+    {
+        $this->descrizione = $descrizione;
+
+        return $this;
+    }
+
+    /**
+     * Get descrizione
+     *
+     * @return string
+     */
+    public function getDescrizione()
+    {
+        return $this->descrizione;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \Application\Sonata\MediaBundle\Entity\Media $image
+     *
+     * @return Articoli
+     */
+    public function setImage(\Application\Sonata\MediaBundle\Entity\Media $image = null)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \Application\Sonata\MediaBundle\Entity\Media
+     */
+    public function getImage()
+    {
+        return $this->image;
     }
 }
